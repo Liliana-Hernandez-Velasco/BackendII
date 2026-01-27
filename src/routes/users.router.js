27f80af0ee70fe04d1e86ef67bbf3e4f3fb18339
Liliana-Hernandez-Velasco <hernandez.velasco.liliana@gmail.com>
2026-01-26 19:41:27 -0600

    Last ones

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,6 +4,7 @@ import { createHash } from "../utils/hash.js";
 
 const router = Router();
 
+// CREATE
 router.post("/register", async (req, res) => {
   try {
     const { first_name, last_name, email, age, password } = req.body;
@@ -24,6 +25,34 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
+});
+
+// READ ALL
+router.get("/", async (req, res) => {
+  const users = await UserModel.find();
+  res.send({ status: "success", users });
+});
+
+// READ ONE
+router.get("/:uid", async (req, res) => {
+  const user = await UserModel.findById(req.params.uid);
+  res.send({ status: "success", user });
+});
+
+// UPDATE
+router.put("/:uid", async (req, res) => {
+  const updatedUser = await UserModel.findByIdAndUpdate(
+    req.params.uid,
+    req.body,
+    { new: true }
+  );
+  res.send({ status: "success", user: updatedUser });
+});
+
+// DELETE
+router.delete("/:uid", async (req, res) => {
+  await UserModel.findByIdAndDelete(req.params.uid);
+  res.send({ status: "success" });
 });
 
 export default router;
