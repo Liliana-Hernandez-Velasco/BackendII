cf3c6d5fdb52c3b763a429409326cda3f8cedd4b
Liliana-Hernandez-Velasco <hernandez.velasco.liliana@gmail.com>
2026-01-26 19:37:53 -0600

    Quick Changes

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -4,22 +4,18 @@ import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.post(
-  "/login",
+router.post("/login",
   passport.authenticate("login", { session: false }),
   (req, res) => {
-    const user = req.user;
+    const token = jwt.sign({ user: req.user }, "coderSecret", { expiresIn: "1h" });
+    res.send({ status: "success", access_token: token });
+  }
+);
 
-    const token = jwt.sign(
-      { user },
-      "coderSecret",
-      { expiresIn: "1h" }
-    );
-
-    res.send({
-      status: "success",
-      access_token: token
-    });
+router.get("/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.send({ status: "success", user: req.user });
   }
 );
 
